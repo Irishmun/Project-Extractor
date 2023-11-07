22ab07afe23f1561f8898dd74377763b26a05818
Irishmun <steefiscool70@gmail.com>
2023-11-07 01:08:57 +0100

    updated to .NET 6, fixed file name issue

--- ProjectExtractor/Settings.cs
+++ ProjectExtractor/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectExtractor.Util;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,6 +8,14 @@ namespace ProjectExtractor
 {
     internal class Settings
     {
+        private const string INI_SECTION_CHAPTERS = "Chapters", INI_SECTION_EXPORT = "Export", INI_SECTION_HOURS = "Hours", INI_SECTION_PATHS = "Paths";
+        private const string INI_KEY_CHAPTER_START = "ChapterStart", INI_KEY_CHAPTER_END = "ChapterEnd";
+        private const string INI_KEY_WRITE_KEYWORDS = "Write_Keywords_To_File", INI_KEY_VERSION = "Project_Version", INI_KEY_EXTENSION = "ExportExtension";
+        private const string INI_KEY_END_PROJECT = "Sections_Project_End", INI_KEY_KEYWORDS = "Keywords", INI_KEY_SECTIONS = "Sections";
+        private const string INI_KEY_WRITE_HOURS = "WriteTotalHours", INI_KEY_TOTAL_HOURS = "TotalHoursKeyword";
+        private const string INI_KEY_SAVE_PDF = "Save_PDF_Path", INI_KEY_SAVE_EXTRACT = "Save_Extract_Path", INI_KEY_DISABLE_EXTRACT = "DisableExtractionPath";
+        private const string INI_KEY_PDF_PATH = "PDF_Path", INI_KEY_EXTRACT_PATH = "Extract_Path";
+
         private const string INI_LIST_SEPARATOR = "; ";
 
         private IniFile _ini;
@@ -36,6 +45,25 @@ namespace ProjectExtractor
         {
             Debug.WriteLine(_iniPath);
             return System.IO.File.Exists(_iniPath);
+        }
+
+        /// <summary>Creates an ini file with default settings</summary>
+        public void CreateDefaultIni(bool savePDF, bool saveExtract, bool disableExtract, bool writeKeywords, bool writeHours, int fileIndex, string fileExtension, string endProject, string chapterStart, string chapterEnd, string totalHoursKey)
+        {
+            //bools
+            UpdateSetting(ref _savePDFPath, savePDF, INI_KEY_SAVE_PDF, INI_SECTION_PATHS);
+            UpdateSetting(ref _saveExtractPath, saveExtract, INI_KEY_SAVE_EXTRACT, INI_SECTION_PATHS);
+            UpdateSetting(ref _disableExtractionPath, disableExtract, INI_KEY_DISABLE_EXTRACT, INI_SECTION_PATHS);
+            UpdateSetting(ref _writeKeywordsToFile, writeKeywords, INI_KEY_WRITE_KEYWORDS, INI_SECTION_EXPORT);
+            UpdateSetting(ref _WriteTotalHours, writeHours, INI_KEY_WRITE_HOURS, INI_SECTION_HOURS);
+            //ints
+            UpdateSetting(ref _selectedFileVersionIndex, fileIndex, INI_KEY_VERSION, INI_SECTION_EXPORT);
+            //strings
+            UpdateSetting(ref _exportFileExtension, fileExtension, INI_KEY_EXTENSION, INI_SECTION_EXPORT);
+            UpdateSetting(ref _sectionsEndProject, endProject, INI_KEY_END_PROJECT, INI_SECTION_EXPORT);
+            UpdateSetting(ref _chapterStart, chapterStart, INI_KEY_CHAPTER_START, INI_SECTION_EXPORT);
+            UpdateSetting(ref _chapterEnd, chapterEnd, INI_KEY_CHAPTER_END, INI_SECTION_EXPORT);
+            UpdateSetting(ref _totalHoursKeyword, totalHoursKey, INI_KEY_TOTAL_HOURS, INI_SECTION_HOURS);
         }
 
         /// <summary>Update the setting value and the setting by key in the ini file, but only if NOT starting</summary>
@@ -93,7 +121,7 @@ namespace ProjectExtractor
         private void InitializeSettings()
         {
             _exportFileExtension = _ini.ReadIfExists("ExportExtension", "Export");//get export file extension
-            _keywords = _ini.ReadArrayIfExists("Keywords", INI_LIST_SEPARATOR, "Export").ToList();//get keywords list 
+            _keywords = _ini.ReadArrayIfExists("Keywords", INI_LIST_SEPARATOR, "Export").ToList();//get keywords list
             _sections = _ini.ReadArrayIfExists("Sections", INI_LIST_SEPARATOR, "Export").ToList();//get sections list
             _sectionsEndProject = _ini.ReadIfExists("Sections_Project_End", "Export");//getsections project end keyword
             _selectedFileVersionIndex = _ini.ReadIntIfExists("Project_Version", "Export");//get project file version combobox index
@@ -166,21 +194,22 @@ namespace ProjectExtractor
             setting = newValue;
             _ini.WriteInt(Key, setting, Section);
         }
-        public bool SavePDFPath { get => _savePDFPath; set => UpdateSettingIfNotStarting(ref _savePDFPath, value, "Save_PDF_Path", "Paths", _isStarting); }
-        public bool SaveExtractPath { get => _saveExtractPath; set => UpdateSettingIfNotStarting(ref _saveExtractPath, value, "Save_Extract_Path", "Paths", _isStarting); }
-        public bool DisableExtractionPath { get => _disableExtractionPath; set => UpdateSettingIfNotStarting(ref _disableExtractionPath, value, "DisableExtractionPath", "Paths", _isStarting); }
-        public bool WriteKeywordsToFile { get => _writeKeywordsToFile; set => UpdateSettingIfNotStarting(ref _writeKeywordsToFile, value, "Write_Keywords_To_File", "Export", _isStarting); }
-        public bool WriteTotalHours { get => _WriteTotalHours; set => UpdateSettingIfNotStarting(ref _WriteTotalHours, value, "WriteTotalHours", "Hours", _isStarting); }
-        public int SelectedFileVersionIndex { get => _selectedFileVersionIndex; set => UpdateSettingIfNotStarting(ref _selectedFileVersionIndex, value, "Project_Version", "Export", _isStarting); }
-        public string ExportFileExtension { get => _exportFileExtension; set => UpdateSettingIfNotStarting(ref _exportFileExtension, value, "ExportExtension", "Export", _isStarting); }
-        public string SectionsEndProject { get => _sectionsEndProject; set => UpdateSettingIfNotStarting(ref _sectionsEndProject, value, "Sections_Project_End", "Export", _isStarting); }
-        public string ChapterStart { get => _chapterStart; set => UpdateSettingIfNotStarting(ref _chapterStart, value, "ChapterStart", "Chapters", _isStarting); }
-        public string ChapterEnd { get => _chapterEnd; set => UpdateSettingIfNotStarting(ref _chapterEnd, value, "ChapterEnd", "Chapters", _isStarting); }
-        public string TotalHoursKeyword { get => _totalHoursKeyword; set => UpdateSettingIfNotStarting(ref _totalHoursKeyword, value, "TotalHoursKeyword", "Hours", _isStarting); }
-        public string PDFPath { get => _PDFPath; set => WriteToOrDeleteFromIniIfNotStarting(ref _PDFPath, value, "PDF_Path", _savePDFPath, "Paths", _isStarting); }
-        public string ExtractionPath { get => _ExtractionPath; set => WriteToOrDeleteFromIniIfNotStarting(ref _ExtractionPath, value, "Extract_Path", _saveExtractPath, "Paths", _isStarting); }
-        public List<string> KeywordsList { get => _keywords; set => UpdateSettingIfNotStarting(ref _keywords, value, "Keywords", "Export", _isStarting); }
-        public List<string> SectionsList { get => _sections; set => UpdateSettingIfNotStarting(ref _sections, value, "Sections", "Export", _isStarting); }
+
+        public bool SavePDFPath { get => _savePDFPath; set => UpdateSettingIfNotStarting(ref _savePDFPath, value, INI_KEY_SAVE_PDF, INI_SECTION_PATHS, _isStarting); }
+        public bool SaveExtractPath { get => _saveExtractPath; set => UpdateSettingIfNotStarting(ref _saveExtractPath, value, INI_KEY_SAVE_EXTRACT, INI_SECTION_PATHS, _isStarting); }
+        public bool DisableExtractionPath { get => _disableExtractionPath; set => UpdateSettingIfNotStarting(ref _disableExtractionPath, value, INI_KEY_DISABLE_EXTRACT, INI_SECTION_PATHS, _isStarting); }
+        public bool WriteKeywordsToFile { get => _writeKeywordsToFile; set => UpdateSettingIfNotStarting(ref _writeKeywordsToFile, value, INI_KEY_WRITE_KEYWORDS, INI_SECTION_EXPORT, _isStarting); }
+        public bool WriteTotalHours { get => _WriteTotalHours; set => UpdateSettingIfNotStarting(ref _WriteTotalHours, value, INI_KEY_WRITE_HOURS, INI_SECTION_HOURS, _isStarting); }
+        public int SelectedFileVersionIndex { get => _selectedFileVersionIndex; set => UpdateSettingIfNotStarting(ref _selectedFileVersionIndex, value, INI_KEY_VERSION, INI_SECTION_EXPORT, _isStarting); }
+        public string ExportFileExtension { get => _exportFileExtension; set => UpdateSettingIfNotStarting(ref _exportFileExtension, value, INI_KEY_EXTENSION, INI_SECTION_EXPORT, _isStarting); }
+        public string SectionsEndProject { get => _sectionsEndProject; set => UpdateSettingIfNotStarting(ref _sectionsEndProject, value, INI_KEY_END_PROJECT, INI_SECTION_EXPORT, _isStarting); }
+        public string ChapterStart { get => _chapterStart; set => UpdateSettingIfNotStarting(ref _chapterStart, value, INI_KEY_CHAPTER_START, INI_SECTION_EXPORT, _isStarting); }
+        public string ChapterEnd { get => _chapterEnd; set => UpdateSettingIfNotStarting(ref _chapterEnd, value, INI_KEY_CHAPTER_END, INI_SECTION_EXPORT, _isStarting); }
+        public string TotalHoursKeyword { get => _totalHoursKeyword; set => UpdateSettingIfNotStarting(ref _totalHoursKeyword, value, INI_KEY_TOTAL_HOURS, INI_SECTION_HOURS, _isStarting); }
+        public string PDFPath { get => _PDFPath; set => WriteToOrDeleteFromIniIfNotStarting(ref _PDFPath, value, INI_KEY_PDF_PATH, _savePDFPath, INI_SECTION_PATHS, _isStarting); }
+        public string ExtractionPath { get => _ExtractionPath; set => WriteToOrDeleteFromIniIfNotStarting(ref _ExtractionPath, value, INI_KEY_EXTRACT_PATH, _saveExtractPath, INI_SECTION_PATHS, _isStarting); }
+        public List<string> KeywordsList { get => _keywords; set => UpdateSettingIfNotStarting(ref _keywords, value, INI_KEY_KEYWORDS, INI_SECTION_EXPORT, _isStarting); }
+        public List<string> SectionsList { get => _sections; set => UpdateSettingIfNotStarting(ref _sections, value, INI_KEY_SECTIONS, INI_SECTION_EXPORT, _isStarting); }
         public string KeywordsString { get => string.Join(INI_LIST_SEPARATOR, _keywords); set => KeywordsList = value.Split(INI_LIST_SEPARATOR).ToList(); }
         public string SectionsString { get => string.Join(INI_LIST_SEPARATOR, _sections); set => SectionsList = value.Split(INI_LIST_SEPARATOR).ToList(); }
 
